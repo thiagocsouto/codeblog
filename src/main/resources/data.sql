4be9530e8a02ae52609da8dd6a5988c723b8c6e1
thiagocsouto <thiagocsouto@outlook.com>
2022-12-29 10:51:11 -0400

    Add teste no codeblog

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
-insert into post(título, autor, data, texto) values
-('EclEmma','Thiago Antonio', PARSEDATETIME('23/07/2022', 'dd/MM/yyyy'),'O EclEmma é uma ferramenta de cobertura de código para Java. De forma resumida, ele te mostra qual a porcentagem do seu código seus testes estão cobrindo.
+insert into post(id, título, autor, data, texto) values
+(1, 'EclEmma','Thiago Antonio', PARSEDATETIME('23/07/2022', 'dd/MM/yyyy'),'O EclEmma é uma ferramenta de cobertura de código para Java. De forma resumida, ele te mostra qual a porcentagem do seu código seus testes estão cobrindo.
  Interessante, não? O JaCoCo disponível para criar sua própria ferramenta.'),
-('Great Time','Thiago Antonio', PARSEDATETIME('20/04/2022', 'dd/MM/yyyy'),'Uma longa história sobre great time.'),
-('teste1','Thiago Antonio', PARSEDATETIME('26/07/2022', 'dd/MM/yyyy'),'Uma teste para o codeblog utilizar.'),
-('teste2','Thiago Antonio', PARSEDATETIME('26/07/2022', 'dd/MM/yyyy'),'Uma teste para o codeblog utilizar.')
-;
+(2, 'Great Time','Luciano Davies', PARSEDATETIME('20/04/2022', 'dd/MM/yyyy'),'Uma longa história sobre great time.'),
+(3, 'New','Green Santos', PARSEDATETIME('26/07/2022', 'dd/MM/yyyy'),'Uma teste para o codeblog utilizar.'),
+(4, 'Legends','Ana da Silva', PARSEDATETIME('26/07/2022', 'dd/MM/yyyy'),'Uma teste para o codeblog utilizar.')
+; 
---
+++ src/test/java/com/blog/codeblog/codeblogServiceTests.java
@@ -0,0 +1,56 @@
+package com.blog.codeblog;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.blog.codeblog.entity.Post;
+import com.blog.codeblog.repository.CodeblogRepository;
+
+
+@DataJpaTest
+public class codeblogServiceTests {
+	
+	@Autowired
+	private CodeblogRepository codeblogRepository;
+	
+	
+	@BeforeEach
+	void initService() {
+	}
+ 
+	@Test
+	void salvarPost() {
+	    
+		Post post = new Post();
+		post.setId(1l); 
+		post.setAutor("Teste");
+		post.setData(LocalDate.now());
+		post.setTitulo("Teste");
+		post.setTexto("Apenas um teste.");
+		
+		codeblogRepository.save(post);
+		
+		assertThat(post.getId()).isGreaterThan(0);
+		
+	}
+	
+	@Test
+	void ListarTodosPost() {
+		
+		List<Post> posts = codeblogRepository.findAll();
+		
+		assertThat(posts.size()).isGreaterThan(0);
+		
+	}
+
+}
