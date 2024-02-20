c24590c966cd09d7ef28f42f2ca881b88342564c
thiago <thiagocsouto@outlook.com>
2024-02-20 17:53:28 -0400

    Ajustes finos nos testes

--- src/test/java/com/blog/codeblog/codeblogServiceTests.java
+++ src/test/java/com/blog/codeblog/codeblogServiceTests.java
@@ -4,13 +4,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDate;
-import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.blog.codeblog.dto.PostDto;
 import com.blog.codeblog.entity.Post;
 import com.blog.codeblog.repository.CodeblogRepository;
 import com.blog.codeblog.service.impl.CodeblogServiceImpl;
@@ -48,40 +48,37 @@ public class codeblogServiceTests {
 	}
 	
 	@Test
-    void error_quando_salva_post_com_texto_nulo() {
-	    
-		//formulário de um post
-		Post post = new Post();
-		post.setId(1l); 
-		post.setAutor("Teste");
-		post.setData(LocalDate.now());  
-		post.setTitulo("Teste");
-		post.setTexto(null);
-		
-		 //acao
-        Exception exception = assertThrows(Exception.class, () -> codeblogServiceImpl.save(post));
-    
-        //asserts
-        assertEquals("Texto não pode ter valor nulo", exception.getMessage());
+	void error_quando_salva_post_com_texto_nulo() {
+	    // Formulário de um post
+	    PostDto postDto = PostDto.builder()
+	            .id(1L)
+	            .autor("Teste")
+	            .data(LocalDate.now())
+	            .titulo("Teste")
+	            .texto(null)
+	            .build();
 
+	    // Ação e assert
+	    assertThrows(Exception.class, () -> codeblogServiceImpl.save(postDto),
+	            "Texto não pode ter valor nulo");
 	}
 	
 	@Test
 	void Listando_um_post() {
 		    
 		//formulário de um post
-		Post post2 = new Post();
-		post2.setId(2l); 
-		post2.setAutor("Teste2");
-		post2.setData(LocalDate.now());  
-		post2.setTitulo("Teste2");
-		post2.setTexto(null);
+		Post post = new Post();
+		post.setId(2l); 
+		post.setAutor("Teste2");
+		post.setData(LocalDate.now());  
+		post.setTitulo("Teste2");
+		post.setTexto(null);
 		
 		//acao
 		codeblogServiceImpl.findById(2l);
 		
 		//asserts
-		assertThat(post2.getId()).isEqualTo(2l);
+		assertThat(post.getId()).isEqualTo(2l);
 		
 	}
 
